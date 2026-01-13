50cec37f5d83f5e92ecf263e75d98c47506c6355
NadineL1 <nadine.sorinder@gmail.com>
2026-01-13 10:04:27 +0100

    Created our entities in models

---
+++ Bean & Code/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bean___Code.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty ;
+        public decimal Price { get; set; }
+        public int CategoryKeyId { get; set; }
+    }
+}
